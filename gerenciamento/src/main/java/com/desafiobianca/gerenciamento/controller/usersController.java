8f1307820475771ed386586daf3d123b4f3bbd8e
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-06-04 18:09:32 -0300

    template html biblioteca thymeleaf-layout-dialect

--- gerenciamento/src/main/java/com/desafiobianca/gerenciamento/controller/usersController.java
+++ gerenciamento/src/main/java/com/desafiobianca/gerenciamento/controller/usersController.java
@@ -35,7 +35,6 @@ public class usersController {
     public String cadastraUser(DadosCadastroUsuario dados){
         var usuario = new Usuario(dados);
         usuarios.add(usuario);
-        System.out.println(usuarios);
 
         return "redirect:/usuarios"; //uso da palavra chave redirect que é um recurdo do spring com a url para redirecionar e então não duplica código do método carregaPaginaListagem. O redirect é via método get, então é como se estivéssemos chamando o carregaPaginaListagem. Então não precisamos também passar o Model novamente e nem editar a variavel lista em vários lugares caso um dia altere algo. 
     }
