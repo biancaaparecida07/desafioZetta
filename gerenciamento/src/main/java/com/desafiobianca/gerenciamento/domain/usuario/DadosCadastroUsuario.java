14cddc83e06c3eee5b2e7c448da33ec58d1ceb8e
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-06-04 17:18:56 -0300

    implementação da data de cadastro dos usuarios

--- gerenciamento/src/main/java/com/desafiobianca/gerenciamento/domain/usuario/DadosCadastroUsuario.java
+++ gerenciamento/src/main/java/com/desafiobianca/gerenciamento/domain/usuario/DadosCadastroUsuario.java
@@ -1,8 +1,9 @@
 package com.desafiobianca.gerenciamento.domain.usuario;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 //O Record foi utilizado por representar uma classe imutável.
-public record DadosCadastroUsuario (String nome, String cpf, Date datanasc, Character sexo, String cargo){
+public record DadosCadastroUsuario (String nome, String cpf, Date datanasc, Character sexo, String cargo, LocalDate dataCadastro){
 
 }
--- gerenciamento/src/main/java/com/desafiobianca/gerenciamento/domain/usuario/Usuario.java
+++ gerenciamento/src/main/java/com/desafiobianca/gerenciamento/domain/usuario/Usuario.java
@@ -1,20 +1,30 @@
 package com.desafiobianca.gerenciamento.domain.usuario;
 
+import java.time.LocalDate;
+
 public class Usuario extends Pessoa{
     private String cargo;
+
+
+    private LocalDate dataCadastro;
 
     public Usuario(DadosCadastroUsuario dados) {
         super(dados.nome(), dados.cpf(), dados.datanasc(), dados.sexo());
         this.cargo = dados.cargo();
+        this.dataCadastro = LocalDate.now();
     }
 
     public String getCargo() {
         return cargo;
     }
 
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
     @Override
     public String toString() {
-        return super.toString()+", cargo=" + cargo + "]";
+        return super.toString()+", cargo=" + cargo + ", Data de Cadastro=" + dataCadastro + "]";
     }
     
 }
