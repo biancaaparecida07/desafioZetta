1ff4fb3e334e643d76cf8931084cbe718c482769
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-06-03 23:15:56 -0300

    implementacao do controller

---
+++ gerenciamento/src/main/java/com/desafiobianca/gerenciamento/controller/usersController.java
@@ -0,0 +1,19 @@
+package com.desafiobianca.gerenciamento.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuarios")
+public class usersController { 
+ 
+    /*Quando uma requisição com a URL /usuarios é recebida, a Servlet do Spring analisa a URL e identifica qual controlador está mapeado para /usuários. */
+    @GetMapping //verbo HTTP
+    public String carregaPaginaFormulario(){
+        return "usuarios/formulario";
+    }
+    
+}
+
+/*A Servlet que aplica o new na classe controladora (usersController) e chama o método carregaPaginaFormulario(). */
