ab47f6d83343fc99416dcf93d1a8cd4b595460be
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-06-04 11:20:08 -0300

    FormularioHTML_postRequest_RecordDadosCadastro

--- gerenciamento/src/main/java/com/desafiobianca/gerenciamento/controller/usersController.java
+++ gerenciamento/src/main/java/com/desafiobianca/gerenciamento/controller/usersController.java
@@ -2,7 +2,10 @@ package com.desafiobianca.gerenciamento.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.desafiobianca.gerenciamento.domain.usuario.DadosCadastroUsuario;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -11,6 +14,13 @@ public class usersController {
     /*Quando uma requisição com a URL /usuarios é recebida, a Servlet do Spring analisa a URL e identifica qual controlador está mapeado para /usuários. */
     @GetMapping //verbo HTTP
     public String carregaPaginaFormulario(){
+        return "usuarios/formulario";
+    }
+
+
+    @PostMapping
+    public String cadastraUser(DadosCadastroUsuario dados){
+        System.out.println(dados);
         return "usuarios/formulario";
     }
     
