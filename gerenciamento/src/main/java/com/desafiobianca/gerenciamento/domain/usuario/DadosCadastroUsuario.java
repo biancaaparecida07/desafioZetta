694cdf770895c9ea6c6aa8781439badcf1d2abc1
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-06-04 15:39:05 -0300

    ajuste do tipo cpf no record

--- gerenciamento/src/main/java/com/desafiobianca/gerenciamento/domain/usuario/DadosCadastroUsuario.java
+++ gerenciamento/src/main/java/com/desafiobianca/gerenciamento/domain/usuario/DadosCadastroUsuario.java
@@ -3,6 +3,6 @@ package com.desafiobianca.gerenciamento.domain.usuario;
 import java.sql.Date;
 
 //O Record foi utilizado por representar uma classe imutável.
-public record DadosCadastroUsuario (String nome, Integer cpf, Date data, Character sexo, String cargo){
+public record DadosCadastroUsuario (String nome, String cpf, Date datanasc, Character sexo, String cargo){
 
 }
